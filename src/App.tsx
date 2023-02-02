c2380662c268f418bbc776fe43f9d55d0898676c
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2023-02-02 10:20:39 +0100

    Adding &  Mapping through todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { InputField } from "./components/InputField";
 import { Todo } from "./components/model";
+import { TodoList } from "./components/TodoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -19,12 +20,18 @@ const App: React.FC = () => {
           isDone: false,
         },
       ]);
+      setTodo("");
     }
   };
+  console.log(todos);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList todos={}/> */}
+      {todos.map((todo) => (
+        <h4>{todo.todo}</h4>
+      ))}
     </div>
   );
 };
