2ae187ccd378acae19e61829628ac6e5fe92f27b
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2023-02-03 09:29:59 +0100

    feat: adding the useReducer

---
+++ src/context/Context.tsx
@@ -0,0 +1,114 @@
+import { useReducer } from "react";
+
+interface Todo {
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+
+//initial state
+const initialState = {
+  todo: "",
+  todos: [],
+  isEditing: false,
+  editedTodo: "",
+};
+// state interface
+interface InitialState {
+  todo: string; //task
+  todos: Todo[]; //lista dyal les task
+  isEditing: boolean;
+  editedTodo: string;
+}
+
+//enum for the action types
+enum ActionType {
+  UPDATE_TODO = "UPDATE_TODO", //the task name state
+  UPDATE_LIST = "UPDATE_LIST", //the array
+  EDIT = "EDIT", //handle the edit
+  DONE = "DONE", //handle the done
+  DELETE = "DELETE", //handle the delete
+  EDITING = "EDITING", //to determine wheter editing ot not
+  EDITED_TODO = "EDITED_TODO", //state of the edited todo task name
+}
+// action interface
+type Action =
+  | { type: ActionType.UPDATE_TODO; payload: string }
+  | { type: ActionType.UPDATE_LIST; payload: Todo[] }
+  | { type: ActionType.EDITING; payload: boolean }
+  | { type: ActionType.EDITED_TODO; payload: string };
+
+const todoReducer = (state: InitialState, action: Action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case ActionType.UPDATE_TODO:
+      return { ...state, todo: payload };
+    case ActionType.UPDATE_LIST:
+      return { ...state, todos: payload };
+    case ActionType.EDITING:
+      return { ...state, isEditing: payload };
+    case ActionType.EDITED_TODO:
+      return { ...state, editedTodo: payload };
+  }
+};
+
+export const Context = () => {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  //handle Add
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (state.todo) {
+      const newList = [
+        ...state.todos,
+        {
+          id: Math.random() * 70,
+          todo: state.todo,
+          isDone: false,
+        },
+      ];
+
+      //add to do to the list
+      dispatch({ type: ActionType.UPDATE_LIST, payload: newList });
+
+      //clear todo message state (also clears the input because of the two ways binding)
+      dispatch({ type: ActionType.UPDATE_TODO, payload: "" });
+    }
+  };
+
+  //handle check
+  const handleDone = (id: number) => {
+    const newList = state.todos.map((todo) =>
+      todo.id === id ? { ...todo, isDone: true } : todo
+    );
+    dispatch({ type: ActionType.UPDATE_LIST, payload: newList });
+  };
+
+  //handle delete
+  const handleDelete = (id: number) => {
+    const newList = state.todos.filter((todo) => todo.id !== id);
+    dispatch({ type: ActionType.UPDATE_LIST, payload: newList });
+  };
+
+  //handle edit
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    const newList = state.todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, todo: state.editedTodo };
+      } else {
+        return todo;
+      }
+    });
+
+    dispatch({ type: ActionType.UPDATE_LIST, payload: newList });
+    dispatch({ type: ActionType.EDITING, payload: false });
+    if (!state.editedTodo) {
+      handleDelete(id);
+    }
+  };
+
+  return <div>context</div>;
+};
